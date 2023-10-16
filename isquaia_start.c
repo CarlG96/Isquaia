09170a80a8aa8bfaaf6ecfbf48455831aa5e6233
CarlG96 <carl.gardiner1@hotmail.com>
2023-10-16 20:19:02 +0100

    added the start of the game

---
+++ isquaia_start.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    printf("Welcome to Isquaia!\n");
+    char name[100];
+    printf("Choose your name:\n");
+    scanf_s("%s", &name);
+    printf("%s\n", name);
+    printf("Choose a race from the following options:\n");
+    printf("1) Isquaian\n");
+    printf("2) Flameborn\n");
+    printf("3) Tideborn\n");
+    printf("4) Cloudborn\n");
+    printf("5) Rockborn\n");
+    printf("6) Ironborn\n");
+    printf("7) Crystalborn\n");
+    printf("8) Forestborn\n");
+    printf("9) Swampborn\n");
+    printf("10) Frostborn\n");
+    printf("11) Songborn\n");
+    printf("12) Stormborn\n");
+    printf("13) Smogborn\n");
+    printf("14) Dustborn\n");
+    printf("15) Steamborn\n");
+    printf("16) Lightborn\n");
+    printf("17) Shadowborn\n");
+    return 0;
+}
